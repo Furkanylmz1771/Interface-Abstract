e8d9c3b8a4a8d00aac0a74141634d7004ee0a6c7
Furkan Yılmaz <92302477+Furkanylmz1771@users.noreply.github.com>
2023-06-02 23:39:58 +0300

    Add files via upload

---
+++ İnterface-Abstarct/İNTERABS.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceAbstract
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Güvenlik güvenlik = new Güvenlik();
+            güvenlik.kimlik();
+            güvenlik.zimmet();
+
+            Şoför şoför = new Şoför();
+            şoför.kimlik();
+            şoför.zimmet();
+
+            Bahcıvan bahcıvan = new Bahcıvan();
+            bahcıvan.zimmet();
+            bahcıvan.kimlik(); 
+        }
+    }
+}
